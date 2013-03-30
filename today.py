2a172745cf8e49dc8e5647a611005b9d85824c60
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-03-30 18:20:22 -0300

    Added sublime-settings

--- today.py
+++ today.py
@@ -1,6 +1,9 @@
 import sublime, sublime_plugin, time;
 
+settings = sublime.load_settings('Today.sublime-settings')
+
 class TodayCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+    format = settings.get('date_format');
     for cursor in self.view.sel():
       self.view.insert(edit, cursor.begin(), time.strftime("%d/%m/%Y"));

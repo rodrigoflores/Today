25f931002137d55bde892347b8fc8246b4ad6553
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-03-31 09:53:25 -0300

    Added now command

--- today.py
+++ today.py
@@ -6,4 +6,10 @@ class TodayCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     format = settings.get('date_format');
     for cursor in self.view.sel():
-      self.view.insert(edit, cursor.begin(), time.strftime("%d/%m/%Y"));
+      self.view.insert(edit, cursor.begin(), time.strftime(format));
+
+class NowCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    format = settings.get('time_format');
+    for cursor in self.view.sel():
+      self.view.insert(edit, cursor.begin(), time.strftime(format));
